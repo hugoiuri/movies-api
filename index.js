6bd8b4fe2d409941d3c07f18eb244e56c0f2b137
hugoiuri <hugoraiuri@gmail.com>
2018-12-23 12:55:46 -0200

    adicionando lib de logs

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const pkg = require('./package.json');
 const server = require('./lib/server');
+const { logger } = require('./lib/logger');
 
 process.title = pkg.name;
 
 const shutdown = async () => {
-  console.info('Gracefully shutdown in progress');
+  logger.info('Gracefully shutdown in progress');
   await server.stop();
   process.exit(0);
 };
@@ -13,23 +14,23 @@ process.on('SIGTERM', shutdown)
   .on('SIGINT', shutdown)
   .on('SIGHUP', shutdown)
   .on('uncaughtException', err => {
-    console.error('uncaughtException caught the error: ', err);
+    logger.error('uncaughtException caught the error: ', err);
     throw err;
   })
   .on('unhandledRejection', (err, promise) => {
-    console.error(`Unhandled Rejection at: Promise ${promise} reason: ${err}`);
+    logger.error(`Unhandled Rejection at: Promise ${promise} reason: ${err}`);
     throw err;
   })
   .on('exit', (code) => {
-    console.info(`Node process exit with code: ${code}`);
+    logger.info(`Node process exit with code: ${code}`);
   });
 
 (async () => {
   try {
     await server.start();
   } catch (err) {
-    console.error('[APP] initialization failed', err);
+    logger.error('[APP] initialization failed', err);
     throw err;
   }
-  console.info('[APP] initialized SUCCESSFULLY');
+  logger.info('[APP] initialized SUCCESSFULLY');
 })();
